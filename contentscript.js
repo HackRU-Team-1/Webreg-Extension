d3eea1f1ac0524436875e6fa47debf2bd8e33e67
gavrieltsioni <gtsioni@gmail.com>
2015-04-19 20:56:37 -0400

    added course schedule planner functionality and made search links open in a new tab automatically

--- contentscript.js
+++ contentscript.js
@@ -13,9 +13,34 @@ for (i = 0; i < x.length; i++) {
 */
 (function refresh() {
     var x = document.getElementsByClassName("instructors");
+	var csp = document.getElementsByTagName("td");
+	
+	var onCSP = false;
+	var cspArr = Array.prototype.slice.call(csp);
+	
+	if(window.location.href.indexOf("/csp/") > -1){
+		//console.log("definitely on csp");
+		onCSP = true;
+	}
+	for(var h = 0; h < cspArr.length; h++){
+		if(cspArr[h].getAttribute("title") == "Instructor"){
+			//console.log("found instructor");
+			continue;
+		}
+		cspArr.splice(h, 1);
+		if(h>0){
+			h--;
+		}
+	}
+	//console.log("post splice:" + cspArr.length);
+	/*
+	if(cspArr.length <= 0){
+		console.log( "onCSP" );		
+		onCSP = true;
+	}*/
     var i;
     console.log( "refreshed" );
-    for (i = 0; i < x.length; i++) {
+    for (i = 0; i < x.length && !onCSP; i++) {
 	    if (x[i].hasChildNodes() && x[i].childNodes.length < 2) {
 			//x[i].style.backgroundColor = "red";
 			//var head = document.getElementsByTagName('head')[0];
@@ -75,7 +100,7 @@ for (i = 0; i < x.length; i++) {
 			
 			
 			link.href = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=rutgers&queryoption=HEADER&query=" + lastName + "&facetSearch=true";
-			
+			link.target = "_blank";
 			/*chrome.runtime.sendMessage({url: link.href}, function(response) {
 				console.log(response.result);
 				console.log("Inside chrome.runtime.sendMessage");
@@ -131,6 +156,128 @@ for (i = 0; i < x.length; i++) {
 			x[i].appendChild(score);
 		}
     }
+	
+	
+	for (i = 0; i < cspArr.length && onCSP; i++) {		
+	    if (cspArr[i].hasChildNodes() && cspArr[i].childNodes.length < 2) {
+			//x[i].style.backgroundColor = "red";
+			//var head = document.getElementsByTagName('head')[0];
+			//var link = document.createElement('link');
+			//link.rel = "stylesheet";
+			//link.href = "stylesheet.css";
+			var score = document.createElement("span");
+			score.id = "score";
+			//score.style.width = "23px";
+			score.style.width = "50px";
+			score.style.height = "24px";
+			//score.style.border = "1px";
+			//score.style.color = "white";
+			score.style.background = "#CF1D32";
+			//score.style.horizontalAlign = "middle";
+			//score.style.verticalAlign = "middle";
+			score.style.paddingTop = "10px";
+			score.style.paddingBottom = "12px";
+			score.style.paddingLeft= "10px";
+			score.style.paddingRight = "10px";
+			score.style.marginLeft = "10px";
+			
+			//score.innerHTML = "5.0";
+			
+			score.style.fontSize = "15px";
+			//score.style.fontFamily = "Alternate-Gothic";
+			//score.innerHTML.font-style = "bold";
+			// score.value = value from API
+			var lastName = cspArr[i].innerHTML;
+			console.log(lastName);
+			console.log(lastName.length);
+			lastName = lastName.toLowerCase();
+			
+			if (lastName == "TBD")
+				continue;
+			
+			var reComma = lastName.split(",",1);
+			if (reComma[0].indexOf(".") != -1) {
+				var rePeriod = lastName.split(".",2); //split on period and space
+				lastName = rePeriod[1]; //This will hold the last name after period and space
+			}
+			else {
+				lastName = reComma[0]; //This will hold the last name before comma (if exists)
+			}
+			
+			if (lastName.charAt(0) == " ") {  //To prevent inconsistencies from Webreg
+				lastName = lastName.substring(1, lastName.length);
+			}
+			
+			var link = document.createElement("a");
+			//link.innerHTML = "5.0";
+			link.innerHTML = "Search";
+			link.style.color = "#FFFFFF";
+			link.style.horizontalAlign = "middle";
+			link.style.verticalAlign = "middle";
+			link.style.textDecoration = "none";
+			link.marginBottom = "10px";
+			
+			
+			
+			
+			link.href = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=rutgers&queryoption=HEADER&query=" + lastName + "&facetSearch=true";
+			link.target = "_blank";
+			/*chrome.runtime.sendMessage({url: link.href}, function(response) {
+				console.log(response.result);
+				console.log("Inside chrome.runtime.sendMessage");
+			});
+			*/
+			/*
+			chrome.runtime.sendMessage({
+				method: 'POST',
+				action: 'xhttp',
+				url: link.href,
+				data: 'q=something'
+			}, function(responseText) {
+				// Callback function to deal with the response
+				link.href = "http://www.ratemyprofessors.com" + responseText;
+				console.log(responseText);
+				score.appendChild(link);
+			});
+			*/
+			/*var xmlhttp = new XMLHttpRequest();
+			xmlhttp.open("GET",link.href, false);
+			
+			xmlhttp.onreadystatechange=function() {
+			  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			  // do stuff here
+			    console.log("Inside onreadystatechange");
+			    var resultString = httpGet(link.href); //holds String of html at href
+				var profUrl = getProfessorUrl(resultString);
+			
+				link.href = "http://www.ratemyprofessors.com" + profURL;
+			  }
+			}
+			*/
+			/*var resultString = httpGet(link.href); //holds String of html at href
+			var profUrl = getProfessorUrl(resultString);
+			
+			link.href = "https://www.ratemyprofessors.com" + responseText;
+			*/
+			score.appendChild(link);
+			
+			
+			/*score.onclick=function(){
+				var win = window.open(text.getchild.href); //PROBLEM HERE: opens link to last professor on page
+				if (win) {
+					win.focus();
+				} else {
+					console.log("heyyyyy");
+				}
+			};*/
+			
+			console.log(lastName);
+			console.log(lastName.length);
+			
+			cspArr[i].appendChild(score);
+		}
+    }	
+	
 	setTimeout(refresh, 1000);
 })();
 
